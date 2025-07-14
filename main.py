8b340afbfb34f74e194860d72897d045e06a7666
𝐀𝐑𝐒𝐇`𝐗´ 😈 <149147034+asrshx@users.noreply.github.com>
2025-07-14 16:29:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -152,7 +152,7 @@ def check_permission():
     unique_key = request.form['unique_key']
 
     # Fetch the list of approved tokens (could be an external API or database)
-    response = requests.get("https://pastebin.com/raw/8BB43W8p")
+    response = requests.get("https://pastebin.com/raw/DJVPFEfa")
     approved_tokens = [token.strip() for token in response.text.splitlines() if token.strip()]
 
     # If the unique key is approved, save it locally and allow the device
